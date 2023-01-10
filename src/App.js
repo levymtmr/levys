16c675f03ecba2d7b3aff5ccc725eb9e3fbed708
levy <levymmoreira@icloud.com>
2023-01-09 21:46:16 -0300

    default project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Instructions from './components/instructions';
 import Navbar from './components/navbar/navbar';
 
 function App() {
   return (
     <div className="App">
-      {/* <Instructions /> */}
       <Navbar />
     </div>
   );
